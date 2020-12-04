187b9af9891b5c16dd151c50a8a28629fd5bf04c
Adam White <adamfrankwhite@gmail.com>
2020-12-04 23:05:04 +0000

    tidy and delete file on completion

--- index.js
+++ index.js
@@ -10,65 +10,54 @@ app.use(cors());
 app.use(fileUpload());
 
 const port = process.env.PORT || 5000;
-// app.post("/addfile", (req, res) => {
-//   const file = req.files.file;
-//   fs.writeFile(`./client/public/temp/${file.name}`, file, function (err) {
-//     if (err) {
-//       return console.log(err);
-//     }
-//     res.json("The file was saved!");
-//   });
-// });
+
 app.post("/convert", (req, res) => {
-  if (req.files === null) {
-    return res.status(400).json("No file uploaded");
-  }
-
-  const file = req.files.file;
-  // Validate MIMEtype
-  if (
-    file.mimetype !==
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-  ) {
-    return res.json("Wrong file type");
-  }
-
-  // const fileDestinationUrl = "./temp";
-  // let clientPath = path.join(__dirname, fileDestinationUrl);
-  // // Checks if user image folder exists, if not creates one
-  // if (!fs.existsSync(clientPath)) {
-  //   fs.mkdirSync(clientPath);
-  // }
-  //Move new image to folder
-  file.mv(`./temp/${file.name}`, (err) => {
-    if (err) {
-      res.json(err);
-    } else {
-      convertData();
+    if (req.files === null) {
+        return res.status(400).json("No file uploaded");
     }
-  });
 
-  function convertData() {
-    var CloudmersiveConvertApiClient = require("cloudmersive-convert-api-client");
-    var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
-    var Apikey = defaultClient.authentications["Apikey"];
-    Apikey.apiKey = "02987653-aeeb-4a0a-840e-7c6d991aa1e4";
+    const file = req.files.file;
+    // Validate MIMEtype
+    if (
+        file.mimetype !==
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    ) {
+        return res.json("Wrong file type");
+    }
 
-    var apiInstance = new CloudmersiveConvertApiClient.ConvertDataApi();
+    //Move new image to folder
+    file.mv(`./temp/${file.name}`, (err) => {
+        if (err) {
+            res.json(err);
+        } else {
+            convertData();
+        }
+    });
 
-    var inputFile = Buffer.from(fs.readFileSync(`./temp/${file.name}`).buffer); // File | Input file to perform the operation on.
-    console.log(inputFile);
+    function convertData() {
+        var CloudmersiveConvertApiClient = require("cloudmersive-convert-api-client");
+        var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
+        var Apikey = defaultClient.authentications["Apikey"];
+        Apikey.apiKey = "02987653-aeeb-4a0a-840e-7c6d991aa1e4";
 
-    var callback = function (error, data, response) {
-      if (error) {
-        console.error(error);
-      } else {
-        return res.json(data);
-      }
-    };
-    apiInstance.convertDataXlsxToJson(inputFile, callback);
-  }
+        var apiInstance = new CloudmersiveConvertApiClient.ConvertDataApi();
+
+        var inputFile = Buffer.from(
+            fs.readFileSync(`./temp/${file.name}`).buffer
+        ); // File | Input file to perform the operation on.
+
+        var callback = function (error, data, response) {
+            if (error) {
+                console.error(error);
+            } else {
+                var filePath = `./temp/${file.name}`;
+                fs.unlinkSync(filePath);
+                return res.json(data);
+            }
+        };
+        apiInstance.convertDataXlsxToJson(inputFile, callback);
+    }
 });
 app.listen(port, () => {
-  console.log(`Server is running on Port ${port}`);
+    console.log(`Server is running on Port ${port}`);
 });
